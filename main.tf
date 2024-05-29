1e674439fa87641d7f4d2715811ae99c4f9d405d
takahiro-hosokawa <takahiro.hosokawa@ctc-g.co.jp>
2024-05-29 06:56:49 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -199,8 +199,8 @@ resource "aws_key_pair" "hashicat" {
 }
 
 
-module "s3-bucket" {
-  source              = "cloudposse/s3-bucket/aws"
+module "s3_bucket" {
+  source  = "app.terraform.io/hands-on-20240529/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
